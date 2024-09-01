1286395679aeec2a85dc31149e0edc42d6ade4c8
Yoel <yoelashkenazi12@gmail.com>
2024-09-02 00:43:04 +0300

    upgraded the scrapper: now with option to have content (currently only in english, still have some issues), and controlled BFS depth via the max amount of links in the initial search. Also made the graph more realistic with same language connections past the BFS step. NOTE- need to update the 'math' datasets

--- main.py
+++ main.py
@@ -9,10 +9,13 @@ if __name__ == '__main__':
                 500: ["Physics", "Physique", "Física"],
                 1000: ["Biology", "Biologie", "Biología"]}
 
-    setup_params = {'langs': LANGS, 'starting_points': subjects[100], 'max_pages_per_lang': 10,
-                    'removal_chance': 0, 'inversion_chance': 0, 'print_info': True}
+    setup_params = {'langs': ['en'], 'starting_points': ['London'], 'max_pages_per_lang': 100,
+                    'removal_chance': 0, 'inversion_chance': 0, 'print_info': True, 'content': True, 'max_links': 5}
     save = True
     filename = 'Excavated Graphs/Mathematics_100_samples_graph.gpickle'
 
     # using scrapper to build the graph
     graph = wiki_scrapper.build_graph(True, save, **setup_params)
+    print("Graph built\n\n")
+    for node in graph.nodes:
+        print(f"Vertex: {node}, Language: '{graph.nodes[node]['lang']}', Content: {graph.nodes[node]['content']}")
--- wiki_scrapper.py
+++ wiki_scrapper.py
@@ -37,13 +37,15 @@ def build_graph(draw: bool = True, save: bool = False, **kwargs):
     print_info = kwargs.get('print_info', False)  # print the process information.
     bigsep = "-" * 100  # large separator.
     smallsep = "-" * 50  # small separator.
+    add_content = kwargs.get('content', False)  # add the content of the pages to the graph.
+    max_bfs_links = kwargs.get('max_links', 5)  # maximum number of links to follow in the BFS.
 
     G = nx.Graph()  # create a graph object.
     red_edges = []  # list to store the red edges.
     blue_edges = []  # list to store the blue edges.
     vertices_in_langs = {lang: set() for lang in languages}  # initialize the vertices in each language.
 
-    # define the vertex colors. choose distinct shapes for each language.
+    """define the vertex colors. choose distinct shapes for each language."""
     shape_dict = {lang: shape for lang, shape in zip(languages, ['s', 'o', '^'])}
     # NOTE-change the shapes list if more languages are added
 
@@ -71,7 +73,8 @@ def build_graph(draw: bool = True, save: bool = False, **kwargs):
             page = pywikibot.Page(site, subject)  # get the page object.
 
             # Get all linked pages in the first section of the page
-            link_titles = get_section_links(page, site)
+            link_titles = get_section_links(page, site)  # get the links.
+            link_titles = link_titles[:min(max_bfs_links, len(link_titles))]  # limit the number of links to follow.
 
             # add neighbors while the number of vertices is less than the maximum.
             while len(vertices_in_lang) < max_pages_per_lang and link_titles:
@@ -86,7 +89,18 @@ def build_graph(draw: bool = True, save: bool = False, **kwargs):
         # Add the vertices to the graph.
         for vertex in vertices_in_lang:
             # add the vertex to the graph.
+            if add_content:
+                text = get_content(vertex)
+                G.add_node(vertex, lang=languages[selected_lang], shape=shape_dict[languages[selected_lang]],
+                           content=text)
+                continue
             G.add_node(vertex, lang=languages[selected_lang], shape=shape_dict[languages[selected_lang]])
+
+        for vertex in vertices_in_lang:  # go over the vertices after adding them to the graph.
+            links = get_section_links(pywikibot.Page(site, vertex), site)  # get the links.
+            for link in links:  # add the red edges.
+                if link in vertices_in_lang:
+                    red_edges.append((vertex, link))
 
         vertices_in_langs[languages[selected_lang]] = vertices_in_lang  # save the vertices in the language.
 
@@ -99,72 +113,81 @@ def build_graph(draw: bool = True, save: bool = False, **kwargs):
     For each vertex in each language, get the translations to the other languages and add to the graph.
     Try to connect the translation to the other vertices in the same language.
     """
-    # Add the translations to the graph.
-    if print_info:
-        print(bigsep)
-        print("Adding translations. Current vertices:", len(G.nodes))
+    if len(languages) > 1:  # if there is more than one language.
+        # Add the translations to the graph.
+        if print_info:
+            print(bigsep)
+            print("Adding translations. Current vertices:", len(G.nodes))
 
-    translations = {lang_: [] for lang_ in languages}  # initialize the translation dictionary. under every language,
-    # it keeps all the translations from other languages to that language.
-    new_vertices = set()
-    for vertex in G.nodes:  # go over the vertices.
-        lang = G.nodes[vertex]['lang']  # get the language of the vertex.
-        site = pywikibot.Site(f'wikipedia:{lang}')  # get the site object.
-        page = pywikibot.Page(site, vertex)  # get the page object.
+        translations = {lang_: [] for lang_ in languages}
+        # initialize the translation dictionary. under every language,
+        # it keeps all the translations from other languages to that language.
+        new_vertices = set()
+        for vertex in G.nodes:  # go over the vertices.
+            lang = G.nodes[vertex]['lang']  # get the language of the vertex.
+            site = pywikibot.Site(f'wikipedia:{lang}')  # get the site object.
+            page = pywikibot.Page(site, vertex)  # get the page object.
 
-        # get the translations of the page.
-        langlinks = page.iterlanglinks()  # get the inter wiki links.
-        langlinks = [link for link in langlinks if link.site.code in languages]  # filter the languages.
-        langlinks = {link.site.code: link.title for link in langlinks if link.site.code in languages}  # filter
+            # get the translations of the page.
+            langlinks = page.iterlanglinks()  # get the inter wiki links.
+            langlinks = [link for link in langlinks if link.site.code in languages]  # filter the languages.
+            langlinks = {link.site.code: link.title for link in langlinks if link.site.code in languages}  # filter
 
-        # add the translations to the graph.
-        for lang_, translation in langlinks.items():
-            new_vertices.add((translation, lang_, shape_dict[lang_]))  # add the translation to the new nodes.
-            blue_edges.append((vertex, translation))  # add the blue edge.
-            translations[lang_].append(translation)  # save the translation to the dictionary.
+            # add the translations to the graph.
+            for lang_, translation in langlinks.items():
+                if add_content:
+                    text = get_content(translation)
+                    new_vertices.add((translation, lang_, shape_dict[lang_], text))  # add the translation to the new nodes.
+                else:
+                    new_vertices.add((translation, lang_, shape_dict[lang_]))  # add the translation to the new nodes.
+                blue_edges.append((vertex, translation))  # add the blue edge.
+                translations[lang_].append(translation)  # save the translation to the dictionary.
 
-    # Add the new nodes to the graph.
-    for v in new_vertices:  # go over the new vertices.
-        G.add_node(v[0], lang=v[1], shape=v[2])  # add the vertex to the graph.
+        # Add the new nodes to the graph.
+        for v in new_vertices:  # go over the new vertices.
+            if add_content:
+                G.add_node(v[0], lang=v[1], shape=v[2], content=v[3])
+                continue
+            G.add_node(v[0], lang=v[1], shape=v[2])  # add the vertex to the graph.
 
-    if print_info:
-        print("Translations added. Current vertices:", len(G.nodes))
+        if print_info:
+            print("Translations added. Current vertices:", len(G.nodes))
 
-    # find red edges between translations in the same language.
-    for lang_, translations in translations.items():
-        site = pywikibot.Site(f'wikipedia:{lang_}')  # get the site object.
-        for translation in translations:  # go over the translations.
-            page = pywikibot.Page(site, translation)  # get the page object.
+        # find red edges between translations in the same language.
+        for lang_, translations in translations.items():
+            site = pywikibot.Site(f'wikipedia:{lang_}')  # get the site object.
+            for translation in translations:  # go over the translations.
+                page = pywikibot.Page(site, translation)  # get the page object.
 
-            # get the links in the first section of the page.
-            link_titles = get_section_links(page, site)  # get the links.
+                # get the links in the first section of the page.
+                link_titles = get_section_links(page, site)  # get the links.
 
-            for title in link_titles:  # go over the links.
-                if title in vertices_in_langs[lang_]:  # if the link is in the vertices.
-                    red_edges.append((translation, title))  # add the red edge.
+                for title in link_titles:  # go over the links.
+                    if title in vertices_in_langs[lang_]:  # if the link is in the vertices.
+                        red_edges.append((translation, title))  # add the red edge.
 
-    # make the blue edges into cliques.
-    for i in range(len(blue_edges)):
-        for j in range(i + 1, len(blue_edges)):
+        # make the blue edges into cliques.
+        for i in range(len(blue_edges)):
             a, b = blue_edges[i]
-            c, d = blue_edges[j]
-            if b == c:
-                blue_edges.append((a, d))
-                continue
-            if a == d:
-                blue_edges.append((c, b))
-                continue
-            if a == c:
-                blue_edges.append((b, d))
-                continue
-            if b == d:
-                blue_edges.append((a, c))
-                continue
+            for j in range(i + 1, len(blue_edges)):
+                c, d = blue_edges[j]
+                if b == c:
+                    blue_edges.append((a, d))
+                    continue
+                if a == d:
+                    blue_edges.append((c, b))
+                    continue
+                if a == c:
+                    blue_edges.append((b, d))
+                    continue
+                if b == d:
+                    blue_edges.append((a, c))
+                    continue
 
-    # remove self-links.
-    for a, b in blue_edges:
-        if a == b:
-            blue_edges.remove((a, b))
+        # remove self-links.
+        for a, b in blue_edges:
+            if a == b:
+                blue_edges.remove((a, b))
 
     # Add the edges to the graph.
     G.add_edges_from(red_edges, color='red')  # add the red edges.
@@ -190,11 +213,6 @@ def build_graph(draw: bool = True, save: bool = False, **kwargs):
         print("Done.")
         print(f"Created a graph with {G.number_of_nodes()} vertices and {G.number_of_edges()} edges.")
         print(bigsep)
-        # temporary: print the blue edges.
-        print("Blue edges:")
-        for edge in G.edges:
-            if G[edge[0]][edge[1]]['color'] == 'blue':
-                print(edge)
 
     """
     Step 4: Draw and save the graph.
@@ -205,7 +223,6 @@ def build_graph(draw: bool = True, save: bool = False, **kwargs):
         pos = nx.spring_layout(G)
         edge_colors = nx.get_edge_attributes(G, 'color').values()
         node_shapes = [shape_dict[G.nodes[node]['lang']] for node in G.nodes]
-        print(node_shapes)
         for shape in node_shapes:  # draw the nodes.
             vertices_to_draw = [node for node in G.nodes if G.nodes[node]['shape'] == shape]
             sizes = [100 for _ in vertices_to_draw]
@@ -225,12 +242,16 @@ def build_graph(draw: bool = True, save: bool = False, **kwargs):
         plt.show()
 
     if save:  # save the graph.
-        filename = f'Excavated Graphs/{starting_points[0]}_{max_pages_per_lang}_samples_graph.gpickle'
+        filename = f'Excavated Graphs/'
+        if add_content:
+            filename += 'With Content/'
+        filename += f'{starting_points[0]}_{max_pages_per_lang}_samples_graph.gpickle'
+        print(f"Saving the graph to '{filename}'")
         try:
             with open(filename, 'wb') as file:
                 pkl.dump(G, file, pkl.HIGHEST_PROTOCOL)
         except OSError:
-            os.makedirs('Excavated Graphs')
+            os.makedirs('Excavated Graphs') if not add_content else os.makedirs('Excavated Graphs/With Content')
             with open(filename, 'wb') as file:
                 pkl.dump(G, file, pkl.HIGHEST_PROTOCOL)
 
@@ -278,8 +299,47 @@ def get_section_links(page: pywikibot.Page, site: pywikibot.Site) -> List:
     sect = textlib.extract_sections(page.text, site)  # extract the sections of the page.
     link_titles = [link.group('title') for link in
                    pywikibot.link_regex.finditer(sect.header)]  # extract the links.
-
     # filter the links.
     link_titles = [link for link in link_titles if not any(char in link for char in BAD_CHARS)]
     link_titles = [link for link in link_titles if link != page.title()]  # remove the self-links.
     return list(link_titles)
+
+
+def get_content(title: str) -> str:
+    """
+    Get the content of a page.
+
+    Args:
+        title (str): The title of the page.
+
+    Returns:
+        str: The content of the page.
+    """
+    # Import packages
+    import wikipedia
+
+    # Specify the title of the Wikipedia page
+    try:
+        wiki = wikipedia.page(title=title)
+    except wikipedia.exceptions.DisambiguationError as e:  # if the page is a disambiguation page.
+        new_title = e.options[0]  # get the first option.
+        try:
+            wiki = wikipedia.page(title=new_title)  # get the page.
+        except wikipedia.exceptions.PageError:  # if the page does not exist.
+            return 'Page does not exist.'
+        except wikipedia.exceptions.DisambiguationError:  # if the page is still a disambiguation page.
+            return 'Disambiguation page.'
+
+    except wikipedia.exceptions.PageError:  # if the page does not exist.
+        return ''
+
+    # Extract the plain text content of the page, excluding images, tables, and other data.
+    text = wiki.content
+
+    # Replace '==' with '' (an empty string)
+    text = text.replace('==', '')
+
+    # Replace '\n' (a new line) with '' & end the string at $500.
+    text = text.replace('\n', '')[:500].split('.')[:-1]  # split the text into sentences.
+    text = '. '.join(text) + '.'  # join the sentences.
+    return text
